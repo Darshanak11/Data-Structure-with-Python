a5d4cb8b5ac46aae262b8670eb908fbe48fc8022
DARSHANA K <darshanak884@gmail.com>
2025-07-18 20:43:22 +0530

    student

---
+++ students.py
@@ -0,0 +1,9 @@
+students = {
+    "Leader": "Alice",
+    "Coder": "Bob",
+    "Designer": "Charlie",
+    "Tester": "David"
+}
+
+for role, name in students.items():
+    print(f"Role: {role}, Name: {name}")
