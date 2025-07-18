579f2c67dc8620232dbb3d4411203ea3ddce0b23
DARSHANA K <darshanak884@gmail.com>
2025-07-18 20:26:52 +0530

    Added README.md

---
+++ list_Reverse.py
@@ -0,0 +1,7 @@
+nums = []
+for _ in range(5):
+    nums.append(int(input("Enter a number: ")))
+
+print("First element:", nums[0])
+print("Last element:", nums[-1])
+print("Reversed list:", nums[::-1])
